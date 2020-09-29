ad7f24d205951452e260ed2c0990a396936fb9f4
agus-barrientos27 <lukas.b2702@outlook.com>
2020-09-29 15:29:32 -0300

    el primer commit de la prueba

---
+++ src/Persona.java
@@ -0,0 +1,21 @@
+
+public class Persona {
+	
+	private int anios;
+	private String nombre;
+	public int getAnios() {
+		return anios;
+	}
+	public void setAnios(int anios) {
+		this.anios = anios;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
