449affaee63223e8636e1b2823addf6ef02fb778
agus-barrientos27 <lukas.b2702@outlook.com>
2020-10-03 01:29:40 -0300

    cambiando clase por carta

---
+++ src/Carta.java
@@ -0,0 +1,29 @@
+
+public class Carta {
+	
+	private String nombre;
+	private int fuerza;
+	private String descripcion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getFuerza() {
+		return fuerza;
+	}
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	
+}
